d6d7e4f7f68e8ea606d88a5250658bc9e9caf9fe
Varunsimha M P <77954226+varunsimha-MP@users.noreply.github.com>
2023-07-01 15:13:40 +0530

    Delete Grocery Management System Project directory

--- Grocery Management System Project/customerlogout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['login']);
-unset($_SESSION['fname']);
-unset($_SESSION['mail']);
-
-header("location:customerlogin.php?logout=true");
-?>
--- Grocery Management System Project/dbconnect2M&C.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost', 'root', '', 'dbmsminiproject');
-
-
-if ($mysqli->connect_error) {
-    die('Error : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-}
-?>
